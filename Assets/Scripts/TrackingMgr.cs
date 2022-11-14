d22b26cd9d722eb5ef21d6f3766dd54ff412e204
jeonse3875 <devbyseke@gmail.com>
2022-11-14 20:30:37 +0900

    기능: 트래킹 이미지 변경 및 트래킹 품질에 따른 활성화/비활성화 기능

---
+++ Assets/Scripts/TrackingMgr.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public enum TrackingImage
+{
+    Door, Window
+}
+
+public class TrackingMgr : MonoBehaviour
+{
+    [SerializeField]
+    ARTrackedImageManager m_TrackedImageManager;
+    public GameObject doorPrefab;
+    public GameObject windowPrefab;
+    private Dictionary<TrackingImage, GameObject> instances = new Dictionary<TrackingImage, GameObject>();
+
+    void OnEnable() => m_TrackedImageManager.trackedImagesChanged += OnChanged;
+
+    void OnDisable() => m_TrackedImageManager.trackedImagesChanged -= OnChanged;
+
+    private void Start() 
+    {
+        instances[TrackingImage.Door] = Instantiate(doorPrefab);
+        instances[TrackingImage.Window] = Instantiate(windowPrefab);
+
+        instances[TrackingImage.Door].SetActive(false);
+        instances[TrackingImage.Window].SetActive(false);
+    }
+
+    void OnChanged(ARTrackedImagesChangedEventArgs eventArgs)
+    {
+        foreach (var newImage in eventArgs.added)
+        {
+            var key = ToEnum(newImage.referenceImage.name);
+            instances[key].transform.position = newImage.transform.position;
+            instances[key].transform.rotation = newImage.transform.rotation;
+            instances[key].transform.SetParent(newImage.transform);
+            instances[key].SetActive(true);
+        }
+
+        foreach (var updatedImage in eventArgs.updated)
+        {
+            var key = ToEnum(updatedImage.referenceImage.name);
+            if (updatedImage.trackingState == TrackingState.None || updatedImage.trackingState == TrackingState.Limited)
+            {
+                instances[key].SetActive(false);
+            }
+            else if (updatedImage.trackingState == TrackingState.Tracking)
+            {
+                instances[key].SetActive(true);
+            }
+        }
+
+        foreach (var removedImage in eventArgs.removed)
+        {
+            var key = ToEnum(removedImage.referenceImage.name);
+            instances[key].SetActive(false);
+        }
+    }
+
+    TrackingImage ToEnum(string str)
+    {
+        return (TrackingImage)Enum.Parse(typeof(TrackingImage), str);
+    }
+}
